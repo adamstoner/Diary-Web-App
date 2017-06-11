cebae87ac737139eb0d371ae83e91636bddcfd7e
Norah Borus <nborus@stanford.edu>
2017-06-10 17:29:20 -0700

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,8 @@
-//
-// TODO(you): Add the JavaScript necessary to complete your final project.
-//
+
+console.log("in script.js");
+let date = new Date();
+const diaryContainer = document.querySelector('#entry');
+const journalId = document.querySelector('title').textContent;
+console.log("In script: Journal Id: ", journalId);
+let dateStr = date.toLocaleString().substr(0, date.toLocaleString().indexOf(','));
+const diary = new DiaryContainer(diaryContainer, dateStr, journalId);
