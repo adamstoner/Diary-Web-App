5f4187aa00e56cbd699db44cb3b41d0753e92843
Norah Borus <nborus@stanford.edu>
2017-06-12 01:35:56 -0700

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,9 @@
 
 console.log("in script.js");
 let date = new Date();
-const diaryContainer = document.querySelector('#entry');
+const diaryContainer = document.querySelector('#entryFlex');
 const journalId = document.querySelector('title').textContent;
-console.log("In script: Journal Id: ", journalId);
+console.log("In script: Journal Id: ", journalId, "date: ", date.toLocaleString());
 let dateStr = date.toLocaleString().substr(0, date.toLocaleString().indexOf(','));
 const diary = new DiaryContainer(diaryContainer, dateStr, journalId);
 initialize(diary);
